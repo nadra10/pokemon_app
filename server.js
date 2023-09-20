c4da22aa1ed987b0baeee4333fa3751222c2e215
nadra10 <125341985+nadra10@users.noreply.github.com>
2023-09-19 23:00:42 -0400

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 const express = require('express');
 const app = express();
-const jsxEngine = require('jsx-view-engine')
+
 
 
 
 const pokemon = require('./models/pokemon.js');
 
-app.get('/', function(req, res){
+app.get('/', (req, res)=>{
     res.send('Welcome to the Pokemon App!');
 })
 
-app.get('/pokemon', function(req, res){
-    res.send(pokemon)
+app.get('/pokemon', (req, res)=>{
+    res.send(pokemon);
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log('listening');
 });
+
+
+
+
+
+
+
+
+
+
+
+
